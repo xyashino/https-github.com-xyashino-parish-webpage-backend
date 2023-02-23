39b18fc75247b089536e578c06bf2a9682e41b90
xyashino <szczypiorpan123@gmail.com>
2023-02-23 15:19:50 +0100

    Update typeorm.config to read base URL from .env file

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,7 +3,6 @@ import {
   TypeOrmModuleOptions,
 } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
-
 export class TypeormConfig {
   static async getOrmConfig(
     configService: ConfigService,
@@ -16,9 +15,9 @@ export class TypeormConfig {
       password: configService.get('DB_PASSWORD'),
       database: configService.get('DB_NAME'),
       entities: [
-        'dist/**/**/**/*.entity{.ts,.js}',
-        'dist/**/**/*.entity{.ts,.js}',
-        'dist/**/*.entity{.ts,.js}',
+        `${configService.get('DB_MIGRATION_BASE')}/**/**/*.entity{.ts,.js}`,
+        `${configService.get('DB_MIGRATION_BASE')}/**/*.entity{.ts,.js}`,
+        `${configService.get('DB_MIGRATION_BASE')}/*.entity{.ts,.js}`,
       ],
       bigNumberStrings: false,
       logging: configService.get('DB_LOGGING') === 'true',
