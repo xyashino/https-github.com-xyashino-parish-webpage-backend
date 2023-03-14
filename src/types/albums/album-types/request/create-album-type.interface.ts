40cf48647177d0393502b24fb37e58d57d169598
xyashino <szczypiorpan123@gmail.com>
2023-03-14 16:51:09 +0100

    Fix types

--- src/types/albums/album-types/request/create-album-type.interface.ts
+++ src/types/albums/album-types/request/create-album-type.interface.ts
@@ -1,4 +1,4 @@
 export interface CreateAlbumType {
-  name: string;
+  name?: string;
   order?: number;
 }
--- src/types/albums/album-types/request/update-album-type.interface.ts
+++ src/types/albums/album-types/request/update-album-type.interface.ts
@@ -1,3 +1,4 @@
 export interface UpdateAlbumType {
-  name: string;
+  name?: string;
+  order?: string;
 }
