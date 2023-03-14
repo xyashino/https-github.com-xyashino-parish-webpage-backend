d682b331876f74ecfc4a1b5748514d41a8103d94
xyashino <szczypiorpan123@gmail.com>
2023-03-14 16:29:15 +0100

    Add order column to update-dto

--- src/albums/dto/update-album-type.dto.ts
+++ src/albums/dto/update-album-type.dto.ts
@@ -1,7 +1,20 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
 
 export class UpdateAlbumTypeDto {
   @IsNotEmpty()
   @IsString()
   name: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(-100)
+  @Max(100)
+  order: number;
 }
